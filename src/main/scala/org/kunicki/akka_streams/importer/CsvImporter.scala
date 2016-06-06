6ba79779c5a7c4d7a1d2401102e3d20ad01ce0fc
Jacek Kunicki <jacek@kunicki.org>
2016-06-06 22:04:01 +0200

    Single line parser

--- src/main/scala/org/kunicki/akka_streams/importer/CsvImporter.scala
+++ src/main/scala/org/kunicki/akka_streams/importer/CsvImporter.scala
@@ -4,7 +4,10 @@ import java.nio.file.Paths
 
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
+import org.kunicki.akka_streams.model.{InvalidReading, Reading, ValidReading}
 import org.kunicki.akka_streams.repository.ReadingRepository
+
+import scala.concurrent.Future
 
 class CsvImporter(config: Config, readingRepository: ReadingRepository) extends LazyLogging {
 
@@ -13,4 +16,17 @@ class CsvImporter(config: Config, readingRepository: ReadingRepository) extends 
   private val concurrentFiles = config.getInt("importer.concurrent-files")
   private val concurrentWrites = config.getInt("importer.concurrent-writes")
   private val nonIOParallelism = config.getInt("importer.non-io-parallelism")
+
+  def parseLine(filePath: String)(line: String): Future[Reading] = Future {
+    val fields = line.split(";")
+    val id = fields(0).toInt
+    try {
+      val value = fields(1).toDouble
+      ValidReading(id, value)
+    } catch {
+      case t: Throwable =>
+        logger.error(s"Unable to parse line in $filePath:\n$line: ${t.getMessage}")
+        InvalidReading(id)
+    }
+  }
 }
